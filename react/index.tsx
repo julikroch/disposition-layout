33dda38ca6b0ade33bddbc83411ba6fb938ea4a1
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-11-09 09:42:30 -0300

    hotfix: site editor

--- react/index.tsx
+++ react/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { ReactChild, ReactElement, ReactNode } from 'react'
+import { ReactChild, ReactNode } from 'react'
 import { NoSSR } from 'vtex.render-runtime'
 
 type Disposition = {
@@ -17,31 +17,24 @@ const DispositionLayout: any = ({
   children,
   disposition,
 }: Props): ReactChild[] | ReactNode => {
-  const array = React.Children.toArray(children) as ReactChild[]
+  const array = React.Children.toArray(children) as any
 
-  const nestedChildren =
-    array.length === 1 ? (array[0] as ReactElement).props.children : null
+  const nestedChildren = array.length === 1 ? array[0].props.children : null
 
-  const sortedChildren = disposition?.reduce((acc, item, i) => {
-    const { order, show } = item
+  const sortedChildren = disposition
+    ?.filter(({ order, show }) => order && show)
+    .map(({ order }) => {
+      if (nestedChildren) {
+        return nestedChildren[order - 1]
+      }
 
-    if (!show) {
-      return acc
-    }
-
-    acc.splice(order - 1, 0, nestedChildren ? nestedChildren[i] : array[i])
-
-    return acc
-  }, [] as ReactChild[])
-
-  if (sortedChildren.length <= 0) {
-    return children
-  }
+      return array[order - 1]
+    })
 
   if (nestedChildren) {
     return [
       {
-        ...(array[0] as ReactElement),
+        ...array[0],
         props: { children: sortedChildren },
       },
     ]
